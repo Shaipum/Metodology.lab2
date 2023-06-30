2ef667d0d58c66a048e4273d0b8a235196dcd5b8
Shaipum <saipumerov@gmail.com>
2023-06-30 11:20:14 +0300

    append class added

--- main.js
+++ main.js
@@ -1,6 +1,30 @@
 "use strict";
 
+const isChar = (elem) => {
+    if (typeof elem !== "string" || elem.length !== 1) {
+      return false;
+    }
+  };
+
   //length implementation
   size() {
     return this.length;
   }
+
+  //append implementation
+  append(elem) {
+    if (isChar(elem) !== false) {
+      const node = new ListNode(elem);
+      if (!this.head) {
+        this.head = node;
+      } else {
+        this.tail.next = node;
+      }
+      this.length++;
+      node.next = this.head;
+      this.tail = node;
+    } else {
+      return;
+    }
+  }
+  
